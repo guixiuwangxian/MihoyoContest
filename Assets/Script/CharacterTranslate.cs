2eeee506b0d5338167229a8e5399b3722225c7d7
wuqiuwangtian <yygqyufanshi@gmail.com>
2025-10-23 15:54:25 +0800

    第一人称实现1.0
    将主相机添加到player中，并实现基础视角随鼠标移动

--- Assets/Script/CharacterTranslate.cs
+++ Assets/Script/CharacterTranslate.cs
@@ -3,35 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class CharacterTranslate: MonoBehaviour
+public class CharacterTranslate : MonoBehaviour
 {
-    public float RotateSpeed=10;
-    public float MoveSpeed = 5;
+    private CharacterController characterController;//角色控制器组件
+    public float RotateSpeed = 10;//旋转速度
+    public float MoveSpeed = 5;//移动速度
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();//获取角色控制器
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");//获取横向输入
+        float v = Input.GetAxis("Vertical");//获取垂直输入
 
         if (h != 0 || v != 0)
         {
 
-            Vector3 dir = new Vector3(h, 0, v);
-            
-            Quaternion targetQ = Quaternion.LookRotation(dir, Vector3.up);
-            targetQ = Quaternion.Lerp(transform.rotation, targetQ, Time.deltaTime*RotateSpeed);
-            transform.rotation = targetQ;
+            Vector3 dir = new Vector3(h, 0, v);//获取向量
+
+            Quaternion targetQ = Quaternion.LookRotation(dir, Vector3.up);//获取目标旋转角度
+            targetQ = Quaternion.Lerp(transform.rotation, targetQ, Time.deltaTime * RotateSpeed);//平滑旋转
+            transform.rotation = targetQ;//人物面向方向
             //向量的移动方法：transform.position=transform.position+transform.forward*Time.deltaTime*MoveSpeed;
-            transform.Translate(transform.forward * Time.deltaTime * MoveSpeed, Space.World);
+            transform.Translate(transform.forward * Time.deltaTime * MoveSpeed, Space.World);//角色控制器移动
         }
     }
-    
-    
+
+
 }
